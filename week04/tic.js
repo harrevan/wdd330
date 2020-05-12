d2cb0b1621b7ba9a018b5eac60a6f27607100d74
Evan Harrison <har16143@byui.edu>
2020-05-12 16:39:51 -0600

    commetns

--- week04/tic.js
+++ week04/tic.js
@@ -1,11 +1,14 @@
+// var to keep track of click counts (to represent player turns)
 let clickCount = 0;
 function turn(){
+    // Alternate betwen X and O turns. X plays first.
     if (clickCount % 2 === 0){
         this.innerHTML = 'X';
     }
     else{
         this.innerHTML = 'O';
     }
+    
     clickCount++;
 }
 function reset(){
@@ -15,4 +18,6 @@ function reset(){
 
 // Add event listener to each cell in table
 document.querySelectorAll('#board td').forEach(e => e.addEventListener("touchend",turn));
+
+// Add event listener for reset button
 document.getElementById('resetButton').addEventListener('touchend', reset);
