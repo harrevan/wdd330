3168de97d4da793513b6cc2664f58c23b26d5822
Evan Harrison <har16143@byui.edu>
2020-05-22 21:48:12 -0600

    ls

--- challenge1/ls.js
+++ challenge1/ls.js
@@ -1,7 +1,5 @@
 export default function saveList(list){
     const toDoListName = "myToDos";
-
-    console.log(list[0]);
-    localStorage.setItem("hello", "hi");
+    localStorage.setItem(toDoListName, JSON.stringify(list));
     console.log(JSON.parse(localStorage.getItem(toDoListName)));
 }
