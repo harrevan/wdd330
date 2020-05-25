d62231c0d46db8e568ca1b38720238f02a6ff92c
Evan Harrison <har16143@byui.edu>
2020-05-25 09:36:34 -0600

    debug

--- challenge1/todos.js
+++ challenge1/todos.js
@@ -29,6 +29,7 @@ export default class ToDo {
     showToDoList(){
         this.parentElement.innerHTML = "";
         renderToDoList(this.parentElement, this.getToDoList());
+        console.log(this.parentElement);
     }
 
 /*     removeToDo(id){
