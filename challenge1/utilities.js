13c0d927a10017291da65e03d073d759eb33c80b
Evan Harrison <har16143@byui.edu>
2020-05-25 08:26:33 -0600

    checks

--- challenge1/utilities.js
+++ challenge1/utilities.js
@@ -1,6 +1,10 @@
 export default function getDOMContent(){
     const content = document.getElementById('addToDo').value;
     if(content != ""){
+        // Empty text input
+        document.getElementById('addToDo').value = "";
+
+        // return content if there is anything
         return content;
     }
 }
