bfebe28911c178b18aa6ded3b34f43599bd658bb
Evan Harrison <har16143@byui.edu>
2020-05-27 17:55:53 -0600

    verify

--- challenge1/todos.js
+++ challenge1/todos.js
@@ -49,9 +49,11 @@ export default class ToDo {
             child.addEventListener("change", e =>{
                 if(this.checked){
                     e.currentTarget.dataset.completed = true;
+                    console.log(e.currentTarget.dataset);
                 }
                 else{
                     e.currentTarget.dataset.completed = false;
+                    console.log(e.currentTarget.dataset);
                 }
             });
 
