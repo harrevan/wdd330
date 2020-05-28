5430bed16e378297065aa95e99be947627260c4e
Evan Harrison <har16143@byui.edu>
2020-05-27 19:44:18 -0600

    asdf

--- challenge1/main.js
+++ challenge1/main.js
@@ -1,10 +1,10 @@
 import ToDo from './todos.js';
 //import Hikes from './todos.js';
 
-const todo = new ToDo();
 const addButton = document.getElementById("add");
 addButton.addEventListener("click", function(){
     //console.log(todo);
+    const todo = new ToDo();
     todo.addToList();
     todo.showToDoList();
 });
