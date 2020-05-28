dcca3a8a9f4b2613b221a71d5b7f57433e93fab4
Evan Harrison <har16143@byui.edu>
2020-05-27 19:56:37 -0600

    sdaf

--- challenge1/ls.js
+++ challenge1/ls.js
@@ -29,7 +29,7 @@ export function updateList(todo){
         }
         console.log(task);
     });
-
+    console.log("retrieve list: " + retrieveList());
     localStorage.setItem(toDoListName, array);
 }
 
